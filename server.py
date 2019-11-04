187ffcc5bbe011dd449ba798a2cdc8c7f3372cfe
xu-jing <xujinghz@umich.edu>
2019-11-03 20:43:27 -0500

    update

--- server.py
+++ server.py
@@ -365,7 +365,7 @@ def resolve_recommendation(clinc_request):
     city = city.capitalize()
     print("city ", city)
     if recommend is None and len(preferences) == 3:
-        url = 'https://www.triposo.com/api/20190906/poi.json?location_id='+city+'&fields=id,name&account=8FRG5L0P&token=i0reis6kqrqd7wi7nnwzhkimvrk9zh6a'
+        url = 'https://www.triposo.com/api/20190906/poi.json?location_id='+city+'&fields=id,name,intro,images&account=8FRG5L0P&token=i0reis6kqrqd7wi7nnwzhkimvrk9zh6a'
         count = 0
         recommend = requests.get(url)
         recommend = recommend.json()
@@ -377,6 +377,8 @@ def resolve_recommendation(clinc_request):
                 {
                     "resolved": 1,
                     "value": recommend['results'][count]['name']
+                    "intro": recommend['results'][count]['intro']
+                    "image": recommend['results'][count]['images'][0]['original']['url']
                 }
             ]
         }
