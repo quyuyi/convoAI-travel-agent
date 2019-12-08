d2fb3e4ccd551a54813a77f895e1d6d891592798
xujinghz <xujinghz@umich.edu>
2019-12-08 13:03:35 -0500

    remove this

--- business_logic_server.py
+++ business_logic_server.py
@@ -737,6 +737,11 @@ def resolve_remove_destination(clinc_request):
         doc_ref.set({
             'sessionId' : user_id
         })
+        
+    try:
+        city = doc_ref.get().to_dict()['city']
+    except:
+        city = "-1"
 
     if clinc_request['slots']:
         destination = capitalize_name(clinc_request['slots']['_DESTINATION_']['values'][0]['tokens'])
