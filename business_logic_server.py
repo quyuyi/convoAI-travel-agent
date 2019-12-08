c1a07f273ff5517fd5f7dfbb8dbe48b9e7d44220
xujinghz <xujinghz@umich.edu>
2019-12-08 13:00:10 -0500

    remove this

--- business_logic_server.py
+++ business_logic_server.py
@@ -741,6 +741,7 @@ def resolve_remove_destination(clinc_request):
     if clinc_request['slots']:
         destination = capitalize_name(clinc_request['slots']['_DESTINATION_']['values'][0]['tokens'])
         last_edit = doc_ref.get().to_dict()['last_edit']
+        city_doc_ref = city_collection.document(city)
         city_recommendations = city_doc_ref.get().to_dict()['recommendations']
 
         mapper_values = {}
